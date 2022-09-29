162f5921732f4d8675dd32b5af33a2ca8525d421
minuee <38347210+minuee@users.noreply.github.com>
2022-09-29 14:50:50 +0900

    first commit

---
+++ src/utils/i18n.ts
@@ -0,0 +1,46 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+import translationKR from '../locales/kr/translation.json';
+import translationEN from '../locales/en/translation.json';
+import translationTR from '../locales/tr/translation.json';
+import translationDE from '../locales/de/translation.json';
+import translationES from '../locales/es/translation.json';
+import translationFR from '../locales/fr/translation.json';
+
+// the translations
+const resources = {
+  kr: {
+    translation: translationKR
+  },
+  en: {
+    translation: translationEN
+  },
+  tr: {
+    translation: translationTR
+  },
+  es: {
+    translation: translationES
+  },
+  fr: {
+    translation: translationFR
+  },
+  de: {
+    translation: translationDE
+  }
+};
+
+i18n
+  .use(initReactI18next as any) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: 'kr',
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    },
+    react: {
+      wait: true
+    }
+  } as any);
+
+export default i18n;
